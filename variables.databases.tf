cc3df96231cda3def3a79c82d6ff6a07c03bc13d
trapeznikov <oleksandr.trapeznikov@gmail.com>
2024-09-17 18:43:24 -0400

    Remove server_id from database variables

--- variables.databases.tf
+++ variables.databases.tf
@@ -3,7 +3,6 @@ variable "databases" {
     charset   = string
     collation = string
     name      = string
-    server_id = string
     timeouts = optional(object({
       create = optional(string)
       delete = optional(string)
@@ -15,7 +14,6 @@ variable "databases" {
  - `charset` - (Required)  Specifies the Charset for the PostgreSQL Database, which needs [to be a valid PostgreSQL Charset](https://www.postgresql.org/docs/current/static/multibyte.html). Changing this forces a new resource to be created.
  - `collation` - (Required) Specifies the Collation for the PostgreSQL Database, which needs [to be a valid PostgreSQL Collation](https://www.postgresql.org/docs/current/static/collation.html). Note that Microsoft uses different [notation](https://msdn.microsoft.com/library/windows/desktop/dd373814.aspx)
  - `name` - (Required) Specifies the name of the PostgreSQL Database, which needs [to be a valid PostgreSQL identifier](https://www.postgresql.org/docs/current/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS). Changing this forces a new resource to be created.
- - `server_id` - (Required) Specifies the resource ID of the PostgreSQL Server. Changing this forces a new resource to be created.
 
  ---
  `timeouts` block supports the following:
