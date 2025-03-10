9777f1ab57824c3dc100c695f11306fb79236283
Asensio, Sergi <sergi.asensio@bossard.com>
2025-03-10 17:13:01 +0100

    Made firewall rules to be nullable if no one is needed

--- main.firewallrule.tf
+++ main.firewallrule.tf
@@ -1,5 +1,5 @@
 resource "azurerm_postgresql_flexible_server_firewall_rule" "this" {
-  for_each = var.firewall_rules
+  for_each = var.firewall_rules != null ? var.firewall_rules : {}
 
   end_ip_address   = each.value.end_ip_address
   name             = each.key
